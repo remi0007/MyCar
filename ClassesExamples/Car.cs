a81539f4d7f051212103b2bc7799a237cf22dfb8
Remi Douah <remidouah@gmail.com>
2020-11-04 12:50:14 -0600

    MyDreamCar

---
+++ ClassesExamples/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExamples
+{
+    public class Car
+
+    {
+       
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ ClassesExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2015;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "Cx5",
+                Year = 2018
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carlist = new List<Car>() { myCar, mazda, chevy };
+
+            Console.WriteLine(carlist.Capacity);
+
+            foreach(var vehicle in carlist)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
